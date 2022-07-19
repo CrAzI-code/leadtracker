907803dbbf970dfd38c4cde07ee13423387fd49f
CrAzI-code <emmaobiora96@gmail.com>
2022-07-19 12:27:10 -0700

    lead tracker

--- leadtrack.js
+++ leadtrack.js
@@ -3,12 +3,21 @@ let myLeads = []
 const inputEl = document.getElementById("input-el")
 const ulEl = document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
+const saveTab = document.getElementById("save-tab")
 
 let inputBtn = document.getElementById("input-btn")
 
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
 
+saveTab.addEventListener("click", function(){ 
+
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setitem("myLeads", JSON.stringify(myLeads))
+        render(myLeads)
+    })
+})
 
 if (leadsFromLocalStorage){
     myLeads = leadsFromLocalStorage
